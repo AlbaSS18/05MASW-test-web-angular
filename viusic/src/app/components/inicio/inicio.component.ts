b2fd62c1ba8cb37b8703645db491afbf8689a687
unknown <albaserenasuarez1998@gmail.com>
2023-02-07 20:56:07 +0100

    init components

--- viusic/src/app/components/inicio/inicio.component.ts
+++ viusic/src/app/components/inicio/inicio.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
+  public titulo: string;
+  public pulsaciones: number;
 
+  constructor() {
+    this.titulo = 'Bienvenido a Viusic';
+    this.pulsaciones = 0;
+  }
+
+  pulsame(): void {
+    this.pulsaciones++;
+  }
+
+  reset(): void {
+    this.pulsaciones = 0;
+  }
 }
