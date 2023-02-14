81bb8e3bac1149995521b76b76bfbf7facf9c561
unknown <albaserenasuarez1998@gmail.com>
2023-02-14 21:20:55 +0100

    add new songs in array

--- viusic/src/app/components/musica/musica.component.ts
+++ viusic/src/app/components/musica/musica.component.ts
@@ -14,11 +14,16 @@ export class MusicaComponent {
     this.records = [
       new Disco (1, 'Reputation', 'Taylor Swift', 2017, 'https://www.lahiguera.net/musicalia/artistas/taylor_swift/disco/8600/taylor_swift_reputation-portada.jpg', 
       [
-        new Song (1, '1', 'I Did Something Bad', 'Taylor Swift', 'https://cdn.pixabay.com/audio/2021/12/22/audio_9da2a60074.mp3', 1)
+        new Song (1, 1, 'I Did Something Bad', 'Taylor Swift', 'https://cdn.pixabay.com/audio/2021/12/22/audio_9da2a60074.mp3', 1)
       ]),
       new Disco (2, 'Entre las dudas y el azar', 'Dani Fernández', 2022, 'https://www.lahiguera.net/musicalia/artistas/dani_fernandez/disco/11710/dani_fernandez_entre_las_dudas_y_el_azar-portada.jpg',
       [
-
+        new Song (2, 1, 'Clima tropical', 'Dani Fernández', 'https://cdn.pixabay.com/audio/2021/12/22/audio_9da2a60074.mp3', 2),
+        new Song (2, 2, 'Plan fatal', 'Dani Fernández', 'https://cdn.pixabay.com/audio/2021/12/22/audio_9da2a60074.mp3', 2),
+        new Song (3, 2, 'Frío', 'Dani Fernández', 'https://cdn.pixabay.com/audio/2021/12/22/audio_9da2a60074.mp3', 2),
+        new Song (4, 2, 'Grace', 'Dani Fernández', 'https://cdn.pixabay.com/audio/2021/12/22/audio_9da2a60074.mp3', 2),
+        new Song (5, 2, 'Dile a los demás', 'Dani Fernández', 'https://cdn.pixabay.com/audio/2021/12/22/audio_9da2a60074.mp3', 2),
+        new Song (6, 2, 'Artificial', 'Dani Fernández', 'https://cdn.pixabay.com/audio/2021/12/22/audio_9da2a60074.mp3', 2),
       ]),
       new Disco (3, 'Motomami', 'Rosalia', 2022, 'https://www.lahiguera.net/musicalia/artistas/rosalia/disco/11741/portada-p.jpg',
       [
