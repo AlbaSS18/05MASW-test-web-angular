1a99e5045f8366e5c03a58d53ea0c124dfc4d8a4
unknown <albaserenasuarez1998@gmail.com>
2023-02-14 20:48:13 +0100

    add model song

---
+++ viusic/src/app/models/song.ts
@@ -0,0 +1,53 @@
+export class Song {
+    constructor(
+        private _id: number,
+        private _track: string,
+        private _title: string,
+        private _artist: string,
+        private _path: string,
+        private _disc_id: number
+    ) {}
+
+    public get id(): number {
+        return this._id;
+    }
+    public set id(value: number) {
+        this._id = value;
+    }
+
+    public get track(): string {
+        return this._track;
+    }
+    public set track(value: string) {
+        this._track = value;
+    }
+
+    public get title(): string {
+        return this._title;
+    }
+    public set title(value: string) {
+        this._title = value;
+    }
+
+    public get artist(): string {
+        return this._artist;
+    }
+    public set artist(value: string) {
+        this._artist = value;
+    }
+
+    public get path(): string {
+        return this._path;
+    }
+    public set path(value: string) {
+        this._path = value;
+    }
+
+    public get disc_id(): number {
+        return this._disc_id;
+    }
+    public set disc_id(value: number) {
+        this._disc_id = value;
+    }
+    
+}
