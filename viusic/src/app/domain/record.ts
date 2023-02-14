bf8f8f0e9e736e56f9b19f239f0dd8604717f9e3
unknown <albaserenasuarez1998@gmail.com>
2023-02-14 20:24:24 +0100

    add ngClass

---
+++ viusic/src/app/domain/record.ts
@@ -0,0 +1,13 @@
+class Disco {
+    title: string;
+    artist: string;
+    year: number;
+    image_path: string;
+
+    constructor(title: string, artist: string, year: number, image_path: string) {
+        this.title = title;
+        this.artist = artist;
+        this.year = year;
+        this.image_path = image_path;
+    }
+}
